4d2bd43e08a1873d3c7fb07e5461fbb7a450ed6d
pratik1100101 <pratik.borude12@gmail.com>
2022-10-20 17:13:11 +0530

    Learning modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
+import { CatsModule } from './cats/cats.module';
 import { CatsService } from './cats/cats.service';
 import { DogsModule } from './dogs/dogs.module';
 
 @Module({
-  imports: [DogsModule],
+  imports: [DogsModule, CatsModule],
   controllers: [AppController, CatsController],
   providers: [AppService, CatsService],
 })
